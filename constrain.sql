7afc2badf52b4668276f318fe35f81e289b410c0
przestrzel96 <113241110+przestrzel96@users.noreply.github.com>
2022-09-10 21:34:22 +0200

    Update constrain.sql

--- constrain.sql
+++ constrain.sql
@@ -1,3 +1,7 @@
 -- dodanie constrainu not null do kolumny Nazwa Druzyny
 alter table DRUZYNA
 modify NAZWA_DRUZYNY not null;
+
+-- dodanie constrainu not null do kolumny IMIE
+alter table PILKARZ
+modify IMIE not null;
