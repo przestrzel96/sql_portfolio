cc854ba96197bc45a41f1446bb0fbca9d5d8aade
przestrzel96 <113241110+przestrzel96@users.noreply.github.com>
2022-09-11 18:03:27 +0200

    Update select.sql

--- select.sql
+++ select.sql
@@ -1,3 +1,4 @@
 -- Przykład 1 (Example 1)
+-- Różnica pensji względem średniej
 select IMIE, NAZWISKO, PENSJA, (select round(avg(PENSJA), 2) from PILKARZ) SREDNIA, ABS(PENSJA - (select round(avg(PENSJA), 2) from PILKARZ)) ROZNICA
 from PILKARZ;
