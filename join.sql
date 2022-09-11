aced044c251e9982582879a8daacce7b44163a57
przestrzel96 <113241110+przestrzel96@users.noreply.github.com>
2022-09-11 15:55:12 +0200

    Update join.sql

--- join.sql
+++ join.sql
@@ -1,1 +1,6 @@
--- przykład 1 
+-- przykład 1 (Example 1)
+select  NAZWA_DRUZYNY, pilkarz.imie as IMIE, pilkarz.nazwisko as NAZWISKO
+from DRUZYNA
+join PILKARZ on DRUZYNA.ID_DRUZYNY = PILKARZ.ID_KLUBU;
+
+
